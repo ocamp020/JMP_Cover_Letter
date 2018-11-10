76da305df3c804bac3486ffa80c4ceb0f12b8fa8
ocamp020 <ocamp020@users.noreply.github.com>
2018-11-09 18:16:35 -0600

    Updated files to include repeat filis

--- Cover_Letters.m
+++ Cover_Letters.m
@@ -46,6 +46,12 @@ for i=1:N
     Institution = strrep(data_aux{1,1},' ','_') ; 
     Position    = strrep(data_aux{1,3},' ','_') ;
     PDF_Title = [Institution{1},'_',Position{1},'_Cover.pdf'];
+        % Check if file already exists (if it does add an index) 
+        j = 1;
+        while exist(PDF_Title, 'file') == 2
+            PDF_Title = [Institution{1},'_',Position{1},'_Cover_',num2str(j),'.pdf'];
+            j = j+1 ;
+        end 
     
     % Change PDF Name
     %eval(['!mv ',PDF_File,' ',PDF_Title,' &'])
