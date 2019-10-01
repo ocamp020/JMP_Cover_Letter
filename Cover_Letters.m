9ba618beb48eaa53aecb0573ffcc10eb88fec550
ocamp020 <ocamp020@users.noreply.github.com>
2019-10-01 15:23:47 +0200

    2019 Update
    Allow for signature in cover letter
    Allow for separate folders by cover letter

--- Cover_Letters.m
+++ Cover_Letters.m
@@ -10,7 +10,7 @@
 % Output:
 % PDFs with cover letter
 %
-% Sergio Ocampo, Nov 7th 2018
+% Sergio Ocampo, Oct 1st 2019
 
 %% Name files
     
@@ -25,6 +25,18 @@
 %% Load institutions and job titles
     Data = readtable(CSV_File)  ; % Three columns: institution, department, position
     N    = size(Data,1)         ; % Number of jobs
+    
+%% Switch for separate folders
+    % If Separate_Folders is set to "true" cover letters are saved in
+    % separate folders containing a tex file for reproduction along with
+    % the pdf. This is useful if further editing (or tailoring) of the
+    % cover letter is required. 
+    
+    % Note: If this is done an additional entry to the CSV file is needed
+    % providing the folder name. The name has to begin with a letter.
+    
+    
+    Separate_Folders = false ; % Alternative value: true
     
 %% Write individual cover letters
 
@@ -42,22 +54,36 @@ for i=1:N
         % [x,y]=system(['pdflatex ',Tex_File]);
     pause(5) %Pause to give the computer time to compile the file before renaminig it 
     
-    % Title for PDF
+    % Title for PDF (and Folder)
     Institution = strrep(data_aux{1,1},' ','_') ; 
     Position    = strrep(data_aux{1,3},' ','_') ;
-    PDF_Title = [Institution{1},'_',Position{1},'_Cover.pdf'];
+    if Separate_Folders
+        Folder    = strrep(data_aux{1,4},' ','_') ;
+        PDF_Title = [Folder{1},'/',Institution{1},'_',Position{1},'_Cover.pdf'];
+        % Create folder if it does not already exists 
+        if exist(Folder{1},'dir') == 0 
+            eval(['!mkdir ',Folder{1}]) 
+        end
+        % Move files to folder for editing 
+        copyfile(CSV_File_aux,[Folder{1},'/',CSV_File_aux])
+        copyfile(Tex_File,[Folder{1},'/',Tex_File])
+        % Comment next line if you are not using a signature
+        copyfile('Signature_File.png',[Folder{1},'/Signature_File.png'])
+    else
+        PDF_Title = [Institution{1},'_',Position{1},'_Cover.pdf'];
         % Check if file already exists (if it does add an index) 
         j = 1;
         while exist(PDF_Title, 'file') == 2
             PDF_Title = [Institution{1},'_',Position{1},'_Cover_',num2str(j),'.pdf'];
             j = j+1 ;
         end 
-    
-    % Change PDF Name
+    end
+
+    % Change PDF Name to PDF_Title (and move to folder if necessary)
     %eval(['!mv ',PDF_File,' ',PDF_Title,' &'])
     movefile(PDF_File,PDF_Title)
-    pause(0.5)
+    pause(1)
     
+    % Report success 
     disp(['Cover Letter for ',Institution{1},' completed'])
 end 
-
